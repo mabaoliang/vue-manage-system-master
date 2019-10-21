2a18b8b1ee5855506e2479d9b605af439abb2c9e
mbl <1032440206@qq.com>
2019-10-21 10:57:42 +0800

    修改A

--- src/components/page/I18n.vue
+++ src/components/page/I18n.vue
@@ -16,6 +16,7 @@
             </div>
             <h2>{{$t('i18n.title2')}}</h2>
             <div>
+
                 <i18n path="i18n.info" tag="p">
                     <a place="action" href="https://element.eleme.cn/2.0/#/zh-CN/component/i18n">{{ $t('i18n.value') }}</a>
                 </i18n>
